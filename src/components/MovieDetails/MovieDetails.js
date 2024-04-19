d6a1425fc9b59f0ddee4b23773ed0df5cc529a21
deanzion <deanzion1@gmail.com>
2024-04-19 21:42:30 +0300

    Modified CSS and installed the FaStar.

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import episodeIdToImage from '@/utils/episodeIdToImage';
+import { FaStar } from 'react-icons/fa'; // Import star icon
 import '@/App.css'; 
 
 function MovieDetails({ movie, onFavoriteToggle, favorites }) {
@@ -10,12 +11,16 @@ function MovieDetails({ movie, onFavoriteToggle, favorites }) {
 
     return (
       <div className="movie-details">
-        <img src={imagePath} alt={movie.title} className="expanded-image" />
-        <div className="favorite-mark" onClick={() => onFavoriteToggle(movie)}>
-          <i className={favorites.some(fav => fav.episode_id === movie.episode_id) ? "fas fa-star" : "far fa-star"}></i>
+        <div className="movie-image-container">
+          <img src={imagePath} alt={movie.title} className="expanded-image" />
+          <div className="movie-title">
+            {movie.title}
+            {/* Render star icon */}
+            <FaStar color={favorites.some(fav => fav.episode_id === movie.episode_id) ? "yellow" : "white"} onClick={() => onFavoriteToggle(movie)} />
+          </div>
         </div>
       </div>
     );
-  }
+}
 
 export default MovieDetails;
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import episodeIdToImage from '@/utils/episodeIdToImage';
+import { FaStar } from 'react-icons/fa'; // Import star icon
 import '@/App.css'; 
 
 const images = require.context('../../images', true);
 
-function MovieItem({ movie, onMovieSelect, onImageClick, isFavorite, onFavoriteToggle }) {
+function MovieItem({ movie, onMovieSelect, onImageClick }) {
   const imageFileName = episodeIdToImage[movie.episode_id];
   const imagePath = images(`./${imageFileName}`);
 
   return (
     <div key={movie.episode_id} className="movie-item">
-      <img src={imagePath} alt={movie.title} onClick={() => {onMovieSelect(movie); onImageClick(movie)}} 
-      className="movie-item img"/>
-      {isFavorite && (
-        <div className="favorite-mark" >
-          <i className="fa fa-star"></i>
-        </div>
-      )}
+      <img src={imagePath} alt={movie.title} onClick={() => {onMovieSelect(movie); onImageClick(movie)}} className="movie-item img"/>
+      <div className="movie-title">
+        {movie.title}
+        {/* Render star icon (hidden on thumbnails) */}
+        <FaStar style={{ visibility: 'hidden' }} />
+      </div>
     </div>
   );
 }
